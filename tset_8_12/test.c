3f0d99a969e5f1ab7daece7009ec600b46c9278f
wuyi13 <138957665+wuyi13@users.noreply.github.com>
2023-08-14 20:02:37 +0800

    Add files via upload

---
+++ tset_8_12/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+////int main()
+////{
+////	int a = 5;
+////	int b = 3;
+////	a = a ^ b;
+////	b = a ^ b;
+////	a = a ^ b;
+////	printf("a=%d\n", a);
+////	printf("b=%d\n", b);
+////		
+////	return 0;
+//}
+//
+
+//1*1=1
+//2*1=2 2*2=4
+//void print(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-3d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}

4c3ae82d1f5428de41400b2ee490762294fb2fd6
wuyi13 <138957665+wuyi13@users.noreply.github.com>
2023-11-16 19:40:47 +0800

    Add files via upload

--- test_2023_11_16/test.c
+++ test_2023_11_16/test.c
@@ -3,16 +3,36 @@
 #include<assert.h>
 //实现strlen函数
 //计数器解法
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str !='\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int len = my_strlen("abcdef");
+//	printf("%d\n", len);
+//	return 0;
+//}
+//不创建临时变量求字符串长度
 int my_strlen(const char* str)
 {
-	int count = 0;
+	
 	assert(str != NULL);
-	while (*str !='\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
+	if (*str != '\0')
+		return 1 + my_strlen(str + 1);
+	else
+		return 0;
+		
+		
+
+	
 }
 int main()
 {
